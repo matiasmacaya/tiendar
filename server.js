f48a9282b37b71745e6b8fb8d50b4f8c0fc70367
Matías Macaya <matiasmacaya@gmail.com>
2024-06-10 14:28:48 +0000

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+const port = 5000;
+
+app.use(cors());
+
+app.use(express.json());
+
+// API routes
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
